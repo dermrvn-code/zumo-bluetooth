906eb2e8177684bd98628124b5fb9c2dae862ef0
Marvin Grunwald <marvingrunwald@aol.com>
2022-05-04 09:38:16 +0200

    Refactor and reorganize

--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,11 @@ Zumo32U4Buzzer buzzer;
 Zumo32U4ProximitySensors proxSensors;
 
 // SETUP BLUETOOTH SERIAL
-int rx_pin = 14; // defining software serial pins (RX and TX) for arduino uno
-int tx_pin = 13;
-SoftwareSerial BluetoothSerial(rx_pin, tx_pin);
+const int RX_PIN = 14;
+const int TX_PIN = 13;
+SoftwareSerial BluetoothSerial(RX_PIN, TX_PIN);
 
+// SPLIT A COMMAND STRING INTO 4 SUBSTRINGS (command word, argument 1-3)
 void splitCommands(String command, String *cmd, String *arg0, String *arg1, String *arg2)
 {
   command += " ";
@@ -42,13 +43,7 @@ void splitCommands(String command, String *cmd, String *arg0, String *arg1, Stri
   command = command.substring(command.indexOf(" ") + 1);
 }
 
-void cmd_m(String arg0, String arg1, String arg2)
-{
-  int leftSpeed = arg0.toInt();
-  int rightSpeed = arg1.toInt();
-  motors.setSpeeds(leftSpeed, rightSpeed);
-}
-
+// SPLITS A COMMAND STRING AND RUNS THE CORRESPONDING ACTION
 void commands(String command)
 {
   String cmd, arg0, arg1, arg2;
@@ -56,9 +51,26 @@ void commands(String command)
 
   if (cmd == "m")
   {
-    cmd_m(arg0, arg1, arg2);
+    int leftSpeed = arg0.toInt();
+    int rightSpeed = arg1.toInt();
+    motors.setSpeeds(leftSpeed, rightSpeed);
   }
 }
+
+// SENDS THE SENSOR DATA INTO THE BLUETOOTH CHANNEL
+void sendSensorData(int frontLeft, int frontRight, int sideLeft, int sideRight)
+{
+  String sd = "sd ";
+  sd += String(frontLeft) + " ";
+  sd += String(frontRight) + " ";
+  sd += String(sideLeft) + " ";
+  sd += String(sideRight);
+  BluetoothSerial.println(sd);
+}
+
+// ======================================================================================
+//
+// ======================================================================================
 
 void setup()
 {
@@ -66,31 +78,39 @@ void setup()
   Serial.begin(9600); // set the data rate for the Hardware Serial port
   Serial.write("Hardware Serial is working !");
 
-  proxSensors.initThreeSensors();
-
   // START BLUETOOTH SERIAL
   BluetoothSerial.begin(9600); // set the data rate for the SoftwareSerial port
   BluetoothSerial.write("Software Serial and Bluetooth is working !");
+
+  // INIT SENSORS
+  proxSensors.initThreeSensors();
 }
 
-char cmds[40];
+// GLOBAL COMMAND VARIABLES
+char cmd_chars[40];
 String cmd = "";
 
 void loop()
 {
+  // ========== READ SENSORS ============================================================
   proxSensors.read();
   int frontLeft = proxSensors.countsFrontWithLeftLeds();
   int frontRight = proxSensors.countsFrontWithRightLeds();
   int sideLeft = proxSensors.countsLeftWithLeftLeds();
   int sideRight = proxSensors.countsRightWithRightLeds();
+  // ====================================================================================
+  //
+  //
+  //
+  // ========== BLUETOOTH IN ============================================================
 
   // BLUETOOTH SIGNAL IS COMING IN
   if (BluetoothSerial.available() && cmd == "")
   {
     // READ CHARS AND WRITE TO COMMAND STRING
-    BluetoothSerial.readBytesUntil(';', cmds, 40);
-    Serial.println(cmds);
-    cmd = String(cmds);
+    BluetoothSerial.readBytesUntil(';', cmd_chars, 40);
+    Serial.println(cmd_chars);
+    cmd = String(cmd_chars);
   }
   else
   {
@@ -103,13 +123,30 @@ void loop()
       delay(100);
     }
   }
+  // ====================================================================================
+  //
+  //
+  //
+  // ========== BLUETOOTH OUT ===========================================================
 
-  // SEND SERIAL DATA TO BLUETOOTH (for debugging)
-  if (Serial.available())
+  if (BluetoothSerial.availableForWrite())
   {
-    BluetoothSerial.write(Serial.read());
-  }
+    // SEND SERIAL DATA TO BLUETOOTH (for debugging)
+    if (Serial.available())
+    {
+      BluetoothSerial.write(Serial.read());
+    }
 
+    sendSensorData(frontLeft, frontRight, sideLeft, sideRight);
+  }
+  // ====================================================================================
+  //
+  //
+  //
+  //
+  // ========== EMERGENCY MECHS =========================================================
+
+  // STOP AND BACKUP IF SOMETHING IN FRONT
   if (frontLeft > 5 || frontRight > 5)
   {
     motors.setSpeeds(-50, -50);
@@ -117,14 +154,5 @@ void loop()
     motors.setSpeeds(0, 0);
   }
 
-  String sd = "sd ";
-  sd.concat(frontLeft);
-  sd.concat(" ");
-  sd.concat(frontRight);
-  sd.concat(" ");
-  sd.concat(sideLeft);
-  sd.concat(" ");
-  sd.concat(sideRight);
-  BluetoothSerial.println(sd);
   delay(50);
 }
